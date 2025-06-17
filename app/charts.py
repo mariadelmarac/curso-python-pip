b21db431455dfa34f8e2233ebdb9d5eaafc280d2
mariadelmaracs <acostasanchezmariadelmar@gmail.com>
2025-06-17 16:53:23 -0500

    App modified

--- app/charts.py
+++ app/charts.py
@@ -1,20 +1,51 @@
 import matplotlib.pyplot as plt
 
 def generate_bar_chart(name, labels, values):
-  fig, ax = plt.subplots()
-  ax.bar(labels, values)
-  plt.savefig(f'./imgs/{name}.png')
+  fig, ax = plt.subplots(figsize=(10, 6))
+  ax.bar(labels, values, color='skyblue')
+  ax.set_xlabel('Year', fontsize=12)
+  ax.set_ylabel('Population', fontsize=12)
+  ax.set_title(f'Population_{name} by Year', fontsize=14)
+
+  ax.set_xticklabels(labels, rotation=45, ha='right', fontsize=10)
+
+  ax.yaxis.grid(True, linestyle='--', alpha=0.7)
+
+  plt.tight_layout()
+
+  plt.savefig(f'./imgs_01/{name}.png', bbox_inches='tight', dpi=300)
   plt.close()
 
-def generate_pie_chart(labels, values):
-  fig, ax = plt.subplots()
-  ax.pie(values, labels=labels)
-  ax.axis('equal')
-  plt.savefig('pie.png')
+def generate_pie_chart(name, labels, values):
+  plt.figure(figsize=(12, 8))
+  explode = [0.05] * len(labels)
+    
+  def autopct_func(percent):
+    return ('%.1f%%' % percent) if percent > 1 else ''
+
+  wedges, texts, autotexts = plt.pie(
+    values,
+    autopct=autopct_func,
+    startangle=90,
+    explode=explode,
+    pctdistance=0.85,
+    textprops={'fontsize': 10}
+  )
+    
+  plt.legend(
+      wedges,
+      labels,
+      title="Countries",
+      loc="center left",
+      bbox_to_anchor=(1, 0, 0.5, 1)
+  )
+  plt.title(f'{name} Population Distribution', fontsize=14, pad=20)
+  plt.axis('equal')
+  plt.savefig(f'./imgs_02/{name}.png', bbox_inches='tight', dpi=300)
   plt.close()
 
 if __name__ == '__main__':
   labels = ['a', 'b', 'c']
   values = [10, 40, 800]
-  # generate_bar_chart(labels, values)
-  generate_pie_chart(labels, values)
+  generate_bar_chart('example_bar', labels, values)
+  generate_pie_chart('example_pie', labels, values)
--- app/main.py
+++ app/main.py
@@ -1,19 +1,21 @@
 import utils
-import read_csv
 import charts
+import pandas as pd
 
 def run():
-  data = read_csv.read_csv('data.csv')
-  data = list(filter(lambda item : item['Continent'] == 'South America',data))
+  continent = input('Type Continent => ').strip().title()
+  print(continent)
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == continent]
 
-  countries = list(map(lambda x: x['Country'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
-  charts.generate_pie_chart(countries, percentages)
-  
-  country = input('Type Country => ')
+  countries = df['Country'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart(f'{continent}_population', countries, percentages)
+
+  country = input('Type Country => ').strip().title()
   print(country)
 
-  result = utils.population_by_country(data, country)
+  result = utils.population_by_country(df, country)
 
   if len(result) > 0:
     country = result[0]
